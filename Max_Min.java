a43c2597e7fc1cac94127de6166fbafe02f1a689
Vivek kumar <89844805+vivekverm@users.noreply.github.com>
2021-11-10 16:11:26 +0530

    Create Max_Min.java

---
+++ Max_Min.java
@@ -0,0 +1,13 @@
+public class Min_Max
+{
+    public static void main(String[] args) {
+        System.out.println("Integer Max_Value:- "+Integer.MAX_VALUE);
+        System.out.println("Integer Min_Value:-"+Integer.MIN_VALUE);
+        System.out.println("Double Max_Value:-"+Double.MAX_VALUE);
+        System.out.println("Double Min_Value:-"+Double.MIN_VALUE);
+        System.out.println("Long Max_Value:-"+Long.MAX_VALUE);
+        System.out.println("Long Min_Value:-"+Long.MIN_VALUE);
+        System.out.println("Short Max_Value:-"+Short.MAX_VALUE);
+        System.out.println("Short Min_Value:-"+Short.MIN_VALUE);
+    }
+}
